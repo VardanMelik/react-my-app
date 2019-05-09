551dc9992e09e78e42ad9ae609dd7c650c5cc0d1
eyemotion <eyemotiongamestudio@gmail.com>
2019-05-09 20:47:55 +0400

    Redux files added

---
+++ src/components/ToDoList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class TodoList extends Component {
+  render() {
+    return {
+      <ul>
+
+      </ul>
+    };
+  }
+}
+
+TodoList.PropTypes = {};
+
+export default TodoList;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+let initialState = [{id: 1, title: 'Learn Redux'}];
+
+let todoReducer = (state = initialState, action) => {
+
+};
+
+export default todoReducer;
